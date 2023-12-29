da30974ec8235e917fa4d63ceab82c85ef256d0d
Csaba Apagyi <csaba.apagyi@gmail.com>
2023-12-29 23:42:54 +0100

    Add activeadmin

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,8 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Needed for ActiveAdmin
+    config.middleware.use ActionDispatch::Flash
   end
 end
