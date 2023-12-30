14fa84e22408fe35579179806ba20d7fca414a50
Csaba Apagyi <csaba.apagyi@gmail.com>
2023-12-30 13:01:29 +0100

    Beautify admin

--- app/admin/asset.rb
+++ app/admin/asset.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Asset do
+  index do
+    column :name do |resource|
+      link_to(resource.name, admin_asset_path(resource))
+    end
+
+    actions
+  end
 end
--- app/admin/currencies.rb
+++ app/admin/currencies.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Currency do
+  index do
+    column :name do |resource|
+      link_to(resource.name, admin_currency_path(resource))
+    end
+
+    actions
+  end
 end
