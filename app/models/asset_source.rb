73b29eb12ab7e9e1c74f150865bf8fa0c6ff58cb
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-01-03 12:15:41 +0100

    Simplify validations

--- app/models/asset_source.rb
+++ app/models/asset_source.rb
@@ -3,8 +3,7 @@
 class AssetSource < ApplicationRecord
   has_many :assets, dependent: :restrict_with_exception
 
-  validates :name, presence: true
-  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[name]
