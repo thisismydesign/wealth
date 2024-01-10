7346231c7a3afc3ecfbe3c767ca6f64d14473edc
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-01-10 13:22:25 +0100

    Fix root

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
-  root to: 'admin#admin'
+  root to: 'admin/dashboard#index'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
