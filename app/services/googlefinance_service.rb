3fb977523fa33fa17b26911cadafc908b441511f
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-01-12 14:23:12 +0100

    Handle sheet id not present

--- app/services/googlefinance_service.rb
+++ app/services/googlefinance_service.rb
@@ -4,12 +4,18 @@ class GooglefinanceService < ApplicationService
   attr_accessor :sheet
 
   def call
+    return if sheet_id.blank?
+
     Faraday.get(url)
   end
 
   private
 
   def url
-    "https://docs.google.com/spreadsheets/d/#{ENV.fetch('GOOGLEFINANCE_SHEET_ID')}/gviz/tq?tqx=out:csv&sheet=#{sheet}"
+    "https://docs.google.com/spreadsheets/d/#{sheet_id}/gviz/tq?tqx=out:csv&sheet=#{sheet}"
+  end
+
+  def sheet_id
+    @sheet_id ||= ENV.fetch('GOOGLEFINANCE_SHEET_ID', nil)
   end
 end
