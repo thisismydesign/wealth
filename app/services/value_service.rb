6273f716171417af7461309c15a480c4d9a607ec
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-01-12 18:28:38 +0100

    Fall back to purchase value when asset is not convertible

--- app/services/value_service.rb
+++ app/services/value_service.rb
@@ -7,8 +7,10 @@ class ValueService < ApplicationService
     return 0 if amount.zero?
     return amount if asset == trade_base_asset
 
-    CurrencyConverterService.call(from: asset, to: trade_base_asset, date: date || Time.zone.today, amount:,
-                                  past_available: true)
+    CurrencyConverterService.call(
+      from: asset, to: trade_base_asset, date: date || Time.zone.today,
+      amount:, past_available: true
+    ) || purchase_value
   end
 
   private
@@ -19,5 +21,14 @@ class ValueService < ApplicationService
 
   def trade_base_asset
     @trade_base_asset ||= Asset.find_by(ticker: Rails.application.config.x.trade_base_currency)
+  end
+
+  def purchase_value
+    Trade.where(to: asset).sum do |trade|
+      CurrencyConverterService.call(
+        from: trade.from, to: trade_base_asset, date: date || Time.zone.today,
+        amount: trade.from_amount, past_available: true
+      )
+    end
   end
 end
