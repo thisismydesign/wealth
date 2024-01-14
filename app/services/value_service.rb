8b55f222edec91f227b477e6752ee0f873c10c83
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-01-14 23:40:15 +0100

    Use Asset.trade_base in value service

--- app/services/value_service.rb
+++ app/services/value_service.rb
@@ -5,24 +5,20 @@ class ValueService < ApplicationService
 
   def call
     return 0 if amount.zero?
-    return amount if asset == trade_base_asset
+    return amount if asset == Asset.trade_base
 
     CurrencyConverterService.call(
-      from: asset, to: trade_base_asset, date: date || Time.zone.today,
+      from: asset, to: Asset.trade_base, date: date || Time.zone.today,
       amount:, fallback_to_past_rate: true
     ) || purchase_value
   end
 
   private
 
-  def trade_base_asset
-    @trade_base_asset ||= Asset.find_by(ticker: Asset.trade_base)
-  end
-
   def purchase_value
     Trade.where(to: asset).sum do |trade|
       CurrencyConverterService.call(
-        from: trade.from, to: trade_base_asset, date: date || Time.zone.today,
+        from: trade.from, to: Asset.trade_base, date: date || Time.zone.today,
         amount: trade.from_amount, fallback_to_past_rate: true
       )
     end
