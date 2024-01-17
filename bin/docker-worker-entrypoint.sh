cc1156c61ceb29ea1eb8bc7ea808dfecee81aa0a
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-01-17 19:18:33 +0100

    Add worker

---
+++ bin/docker-worker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+wait_for()
+{
+  echo "Waiting $1 seconds for $2:$3"
+  timeout $1 sh -c 'until nc -z $0 $1; do sleep 0.1; done' $2 $3 || return 1
+  echo "$2:$3 available"
+}
+
+bundle check || bundle install
+
+wait_for 10 db 5432
+wait_for 10 redis 6379
+
+exec "$@"
