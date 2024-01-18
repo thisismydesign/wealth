9d7a4956e14a0864cdf43e02b1e60a368d22b239
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-01-18 18:01:54 +0100

    Fix specs

--- spec/services/total_income_service_spec.rb
+++ spec/services/total_income_service_spec.rb
@@ -10,9 +10,13 @@ RSpec.describe TotalIncomeService do
   end
 
   context 'when there are incomes' do
-    before { create_list(:income, 2, amount: 1) }
+    before do
+      create_list(:income, 2, amount: 1).each do |income|
+        create(:price, priceable: income, amount: 1, asset: Asset.tax_base)
+      end
+    end
 
-    it 'returns the sum of all incomes converted' do
+    it 'returns the sum of all income prices' do
       allow(CurrencyConverterService).to receive(:call).and_return(1)
 
       expect(call).to eq(2)
