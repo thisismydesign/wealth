77a24fd48b23c58cb2e457ab5b5aea211522fb34
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-01-23 00:13:21 +0100

    Import forex trades from ibkr

--- app/services/import_activity_from_ibkr_service.rb
+++ app/services/import_activity_from_ibkr_service.rb
@@ -3,10 +3,13 @@
 class ImportActivityFromIbkrService < ApplicationService
   attr_accessor :csv_file
 
+  # rubocop:disable Metrics/MethodLength
   def call
     CSV.foreach(csv_file.path, headers: false, liberal_parsing: true) do |row|
-      if trade?(row)
-        import_trade(row)
+      if currency_trade?(row)
+        import_currency_trade(row)
+      elsif stock_trade?(row)
+        import_stock_trade(row)
       elsif funding?(row)
         import_funding(row)
       elsif dividend?(row)
@@ -14,23 +17,51 @@ class ImportActivityFromIbkrService < ApplicationService
       end
     end
   end
+  # rubocop:enable Metrics/MethodLength
 
   private
 
-  def trade?(row)
-    row[0] == 'Trades' && row[1] == 'Data' && row[2] == 'Order'
+  def currency_trade?(row)
+    row[0] == 'Trades' && row[1] == 'Data' && row[2] == 'Order' && row[3] == 'Forex'
   end
 
-  def import_trade(row)
+  def import_currency_trade(row)
+    to_ticker, from_ticker = row[5].split('.')
+    from = ensure_asset(from_ticker)
+    to = ensure_asset(to_ticker)
+
+    return unless from && to
+
+    Trade.where(row_to_currency_trade(row, from, to)).first_or_create!
+  end
+
+  def row_to_currency_trade(row, from, to)
+    to_amount = to_big_decimal(row[7])
+    from_amount = to_big_decimal(row[10])
+
+    if to_amount.positive?
+      # Buy
+      { from:, to:, date: row[6], to_amount:, from_amount: from_amount.abs, asset_holder: }
+    else
+      # Sell
+      { from: to, to: from, date: row[6], to_amount: from_amount, from_amount: to_amount.abs, asset_holder: }
+    end
+  end
+
+  def stock_trade?(row)
+    row[0] == 'Trades' && row[1] == 'Data' && row[2] == 'Order' && row[3] == 'Stocks'
+  end
+
+  def import_stock_trade(row)
     from = ensure_asset(row[4])
     to = ensure_asset(row[5])
 
     return unless from && to
 
-    Trade.where(row_to_trade(row, from, to)).first_or_create!
+    Trade.where(row_to_stack_trade(row, from, to)).first_or_create!
   end
 
-  def row_to_trade(row, from, to)
+  def row_to_stack_trade(row, from, to)
     to_amount = to_big_decimal(row[7])
     from_amount = to_big_decimal(row[10]) + to_big_decimal(row[11])
 
--- spec/services/import_activity_from_ibkr_service_spec.rb
+++ spec/services/import_activity_from_ibkr_service_spec.rb
@@ -5,13 +5,13 @@ require 'rails_helper'
 RSpec.describe ImportActivityFromIbkrService do
   subject(:call) { described_class.call(csv_file:) }
 
-  context 'when export contains a spend & receive pair' do
+  context 'when export contains a stock trade' do
     before do
       create(:asset, ticker: 'USD')
       create(:asset, ticker: 'NDIA')
     end
 
-    let(:csv_file) { fixture_file_upload(Rails.root.join('spec/fixtures/ibkr_trade.csv'), 'text/csv') }
+    let(:csv_file) { fixture_file_upload(Rails.root.join('spec/fixtures/ibkr_stock_trade.csv'), 'text/csv') }
 
     it 'creates trades' do
       expect { call }.to change(Trade, :count).by(2)
@@ -32,6 +32,28 @@ RSpec.describe ImportActivityFromIbkrService do
       expect(Trade.last).to have_attributes(
         from_amount: BigDecimal('2200'), from: Asset.find_by(ticker: 'NDIA'), to_amount: BigDecimal('18677.436897755'),
         to: Asset.find_by(ticker: 'USD'), date: Time.zone.parse('2023-12-27, 03:00:21')
+      )
+    end
+  end
+
+  context 'when export contains a forex trade' do
+    before do
+      create(:asset, ticker: 'USD')
+      create(:asset, ticker: 'EUR')
+    end
+
+    let(:csv_file) { fixture_file_upload(Rails.root.join('spec/fixtures/ibkr_forex_trade.csv'), 'text/csv') }
+
+    it 'creates trades' do
+      expect { call }.to change(Trade, :count).by(1)
+    end
+
+    it 'creates trade with correct attributes' do
+      call
+
+      expect(Trade.first).to have_attributes(
+        from_amount: BigDecimal('14996.33'), from: Asset.find_by(ticker: 'EUR'), to_amount: BigDecimal('16372.993094'),
+        to: Asset.find_by(ticker: 'USD'), date: Time.zone.parse('2023-06-29, 04:01:30')
       )
     end
   end
