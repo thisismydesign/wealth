693bf6812d472619812ba5c0ec0e8adedbc222f6
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-05-24 15:48:39 +0200

    Fix rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -4,7 +4,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = Rails.application.credentials.dig(Rails.env.to_sym, :rollbar, :api_key)
+  config.access_token = Rails.application.credentials.dig(Rails.env.to_sym, :rollbar, :api_token)
   config.enabled = Rails.env.production?
 
   # By default, Rollbar will try to call the `current_user` controller method
