63cbb7014f406ac8fa0cd6f8857b8642927c277a
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-05-31 17:42:52 +0200

    Explain rare issue

--- spec/factories/assets.rb
+++ spec/factories/assets.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :asset do
     name { Faker::Currency.unique.name }
-    ticker { Faker::Currency.unique.code }
+    ticker { Faker::Currency.unique.code } # This can sometimes fail, possibly due to validation being case insensitive
     asset_type
   end
 end
