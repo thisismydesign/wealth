06577a82e7013637b8d818915ea12fb27095c097
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-11-12 12:59:19 +0100

    Fix exchange rate message

--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -4,7 +4,7 @@ class ExchangeRate < ApplicationRecord
   belongs_to :from, class_name: 'Asset'
   belongs_to :to, class_name: 'Asset'
 
-  validates :date, uniqueness: { scope: %i[from_id to_id], message: I18n.t('models.asset.exchange_rate.uniqueness') }
+  validates :date, uniqueness: { scope: %i[from_id to_id], message: I18n.t('exchange_rate.uniqueness') }
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[amount]
