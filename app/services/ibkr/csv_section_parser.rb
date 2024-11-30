d8d3da437f6a36ce6a2b0f09ee023ee46624d483
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-11-30 15:58:20 +0100

    Refactor CsvSectionParser

--- app/services/ibkr/csv_section_parser.rb
+++ app/services/ibkr/csv_section_parser.rb
@@ -5,21 +5,40 @@ module Ibkr
     attr_accessor :csv_file, :section
 
     def call
-      headers = nil
+      headers = find_section_headers
+      collect_section_data(headers)
+    end
+
+    private
+
+    def find_section_headers
+      CSV.foreach(csv_file.path, headers: false, liberal_parsing: true) do |row|
+        return row[2..] if section_header_row?(row)
+      end
+    end
+
+    def collect_section_data(headers)
       section_data = []
 
       CSV.foreach(csv_file.path, headers: false, liberal_parsing: true) do |row|
-        headers = row[2..] if row[0] == section && row[1] == 'Header'
-      end
+        next unless section_data_row?(row)
 
-      CSV.foreach(csv_file.path, headers: false, liberal_parsing: true) do |row|
-        if row[0] == section && row[1] == 'Data'
-          data = row[2..]
-          section_data << headers.zip(data).to_h
-        end
+        section_data << create_data_hash(headers, row[2..])
       end
 
       section_data
+    end
+
+    def section_header_row?(row)
+      row[0] == section && row[1] == 'Header'
+    end
+
+    def section_data_row?(row)
+      row[0] == section && row[1] == 'Data'
+    end
+
+    def create_data_hash(headers, data)
+      headers.zip(data).to_h
     end
   end
 end
