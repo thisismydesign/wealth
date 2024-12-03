5f8a6afc7b8d0abed524740ae7373a795d35d92b
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-12-03 20:59:56 +0100

    Add users

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User do
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+end
