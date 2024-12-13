bf0e4077a6ab77e4d492bb6d07cdfd3d5c4708d6
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-12-13 17:17:47 +0100

    Upgrade to rails 8

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'importmap-rails' # Use JavaScript with ESM import maps [https://github.com/
 gem 'pg', '~> 1.1'
 gem 'propshaft' # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
 gem 'puma', '>= 5.0'
-gem 'rails', '~> 7.1.2'
+gem 'rails', '~> 8.0.0'
 gem 'stimulus-rails' # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
 gem 'turbo-rails' # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
 gem 'tzinfo-data', platforms: %i[windows jruby]
--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ Bundler.require(*Rails.groups)
 module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -38,10 +38,8 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    # Don't generate system test files.
+    config.generators.system_tests = nil
 
     # Needed for ActiveAdmin
     config.middleware.use ActionDispatch::Flash
