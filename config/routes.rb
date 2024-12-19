18b3a06baabc09f27b27883b48d405b7f31dd0b7
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-12-19 17:41:41 +0100

    Auth users to be able to access background jobs

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,15 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root to: 'admin/dashboard#index'
+  root to: 'admin/tax#index'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  get 'imports/exchange_rates_from_mnb', to: 'imports#exchange_rates_from_mnb'
-  get 'imports/rates_from_googlefinance', to: 'imports#rates_from_googlefinance'
-  post 'imports/activity_from_ibkr', to: 'imports#activity_from_ibkr'
   post 'imports/activity_from_kraken', to: 'imports#activity_from_kraken'
-  post 'imports/activity_from_wise', to: 'imports#activity_from_wise'
 
-  mount GoodJob::Engine => 'good_job'
+  authenticate :user, ->(u) { u.admin? } do
+    mount GoodJob::Engine => 'good_job'
+  end
 end
