e3f8c0d8d916aaf9534585e922d101ae09d523ce
Csaba Apagyi <csaba.apagyi@gmail.com>
2024-12-19 20:15:58 +0100

    Specify that kraken import is from csv

--- app/admin/import.rb
+++ app/admin/import.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register_page 'Import' do
   menu priority: 13
 
   content do
-    panel 'Import activity from Kraken' do
+    panel 'Import Kraken CSV' do
       render 'admin/shared/csv_import_form', path: imports_activity_from_kraken_path
     end
 
