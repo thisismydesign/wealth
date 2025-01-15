b50d38bcdce762e214cf2dca054dad2bf7b90a4e
Csaba Apagyi <csaba.apagyi@gmail.com>
2025-01-15 22:11:49 +0100

    Remove unused action

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,10 +3,6 @@
 ActiveAdmin.register_page 'Dashboard' do
   menu priority: 11, label: proc { I18n.t('active_admin.dashboard') }
 
-  # action_item :imports_exchange_rates_from_mnb, only: :index do
-  #   link_to 'Import exchange rates from MNB', imports_exchange_rates_from_mnb_path
-  # end
-
   content title: proc { I18n.t('active_admin.dashboard') } do
     asset_balances = TotalBalancesService.call(user: current_user)
 
