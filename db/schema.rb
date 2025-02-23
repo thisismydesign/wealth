f8927b9987bee698de643da03c09b3a9e61e56cd
Csaba Apagyi <csaba.apagyi@gmail.com>
2025-02-23 01:09:46 +0100

    Regenrate schema files

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_02_17_132301) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
   create_table "asset_holders", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
